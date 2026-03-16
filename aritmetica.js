733c4d50d1ffc3bfa19ed948d4294b3921a5cb3c
Alex <202112087@est.umss.edu>
2026-03-16 19:20:40 -0400

    multiplicacion Alex Nina Copatiti

--- aritmetica.js
+++ aritmetica.js
@@ -11,3 +11,8 @@ let b = 5;
 
 console.log("La resta es: " + restaAlejandroAmaruCondoriMorales(a, b));
 console.log("La suma es: " + sumaJoaquinOctavioTapiaVera(a, b));
+
+function multiplicacionAlexNinaCopatiti(a,b){
+    return a * b;
+}
+console.log("la multiplicacion es:" + multiplicacionAlexNinaCopatiti(a,b));
