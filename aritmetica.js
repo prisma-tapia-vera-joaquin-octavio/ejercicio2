715edfdf9965a229c855cbd60d90a6959b7d981e
aacm <pxe592@gmail.com>
2026-03-16 18:02:41 -0400

    añadida la llamada al metodo de resta

--- aritmetica.js
+++ aritmetica.js
@@ -5,3 +5,8 @@ function sumJoaquinOctavioTapiaVera(firstNumber, secondNumber){
 function restaAlejandroAmaruCondoriMorales(a, b){
     return a-b;
 }
+
+let a = 10;
+let b = 5;
+
+console.log("La resta es: " + restaAlejandroAmaruCondoriMorales(a, b));
