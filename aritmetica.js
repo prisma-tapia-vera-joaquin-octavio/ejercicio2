af1087490c534c9667045d8775bfc5310d6750b8
wil9889 <awm9829@gmail.com>
2026-03-16 20:12:29 -0400

    Wilber Ojeda Valente

--- aritmetica.js
+++ aritmetica.js
@@ -6,8 +6,13 @@ function restaAlejandroAmaruCondoriMorales(a, b){
     return a-b;
 }
 
+function moduloWilberOjedaValente1(a, b){
+    return a % b;
+}
+
 let a = 10;
 let b = 5;
 
 console.log("La resta es: " + restaAlejandroAmaruCondoriMorales(a, b));
 console.log("La suma es: " + sumaJoaquinOctavioTapiaVera(a, b));
+console.log("El modulo es: " + moduloWilberOjedaValente1(a, b));
